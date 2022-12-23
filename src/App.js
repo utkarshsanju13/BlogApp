a99729267650ba3ee4e36864f7db07840f2de961
utkarshsanju13 <utkarshsanju13@gmail.com>
2022-12-23 14:14:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import Layout from "./components/Layout";
+import { Route, Routes } from 'react-router-dom'; 
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+
+        <Route index element={<PostsList/>}/>
+
+        <Route path = "post">
+          <Route index element={<AddPostForm />}/>
+          <Route path=":postId" element={<SinglePostPage/>}/>
+          <Route path="edit/:postId" element={<EditPostForm />}/>
+
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
